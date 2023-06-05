8f99cb5f06c7a74402c819cf154c5234c7602bc9
JRosSx91 <hazee.1491@gmail.com>
2023-06-05 17:52:38 +0200

    add particle struct

--- gravity3d/src/main.rs
+++ gravity3d/src/main.rs
@@ -2,6 +2,15 @@ use three::controls::OrbitControls;
 use three::window::Window;
 use three::Object;
 
+struct Particle {
+    x: f32,
+    y: f32,
+    z: f32,
+    speed_x: f32,
+    speed_y: f32,
+    speed_z: f32,
+    mass: f32,
+}
 fn main() {
     println!("Hello, world!");
 }
