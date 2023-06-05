7569fa55b8d040bf56fc9b8e7e134a221aa8e19e
JRosSx91 <hazee.1491@gmail.com>
2023-06-05 17:25:34 +0200

    create new project

---
+++ gravity3d/target/debug/deps/gravity3d-a92b5f39c18f8a84.d
@@ -0,0 +1,5 @@
+C:\Users\Ivan\labs\rust\3D-Gravity-Game\gravity3d\target\debug\deps\gravity3d-a92b5f39c18f8a84.rmeta: src\main.rs
+
+C:\Users\Ivan\labs\rust\3D-Gravity-Game\gravity3d\target\debug\deps\gravity3d-a92b5f39c18f8a84.d: src\main.rs
+
+src\main.rs:
